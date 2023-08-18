2da133f0acf008f525bdeeac495c4f2c7f3832bf
adam-tricarico <123761295+adam-tricarico@users.noreply.github.com>
2023-08-18 09:50:57 -0400

    Add Comments for Clarity

--- NLP.py
+++ NLP.py
@@ -1,18 +1,22 @@
+# Import libraries
 import nltk
 import pandas as pd
 from nltk.metrics.distance import jaccard_distance
 from nltk.util import ngrams
 
+# Create jaccard_similarity function to get jaccard_distance
 def jaccard_similarity(x,y):
 
     return 1 - jaccard_distance(x,y)
 
+# N-Grams Function
 def get_similarity_by_n_grams(left, right, n):
     seq1 = set(nltk.word_tokenize(left, n=n))
     seq2 = set(nltk.word_tokenize(right, n=n))
 
     similarity = 1 - jaccard_distance(seq1, seq2)
 
+# Create row function to find most similar client
 def create_row(client1, client2, df):
     row = []
     sequence1 = set(client1)
@@ -28,17 +32,21 @@ def create_row(client1, client2, df):
 input1 = ["P. Poll", "Petal & Cat", "Nuckerruck", "Princess Shiane"]
 input2 = ["Polly P."]
 
+# Create final_rows list
 final_rows = []
 
+# For loop to iterate through list of clients
 for client1 in input2:
+
+# Create rows list
     rows = []
 
+# Lambda function to collect every client's most compatible name
     rows = list(map(lambda client2: create_row(client1, client2, df), input1))
 
+# Sort rows by jaccard_distance
     rows.sort(key = lambda row: row[2])
+    
+# Append final_rows with the Most compatible collection of data
     final_rows.append(rows[-1])
     final_rows.sort(key=lambda row: row[2])
-
-for row in final_rows:
-
-    final_df = pd.DataFrame(final_rows, columns = ["Client Name", "Closest Client Name", "Similarity", "Volume"])
