2cc3b8590f5b5259148d45a2e30de76b591c6fd4
adam-tricarico <123761295+adam-tricarico@users.noreply.github.com>
2023-08-28 10:08:17 -0400

    Add new libraries

--- NLP.py
+++ NLP.py
@@ -1,8 +1,11 @@
 # Import libraries
 import nltk
 import pandas as pd
+import numpy as np
 from nltk.metrics.distance import jaccard_distance
 from nltk.util import ngrams
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 # Create jaccard_similarity function to get jaccard_distance
 def jaccard_similarity(x,y):
